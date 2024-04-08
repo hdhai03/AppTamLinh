d65d1fac70894a51ea1ada0fb22359051fddde7a
hdhai03 <duchai2912@gmail.com>
2024-04-08 22:08:45 +0700

    Màn hình đăng nhập

--- app/src/main/java/com/example/apptamlinh/MainActivity.java
+++ app/src/main/java/com/example/apptamlinh/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.apptamlinh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnDangNhap;
+    private Button btnDangKy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnDangNhap = findViewById(R.id.btn_DangNhap);
+        btnDangKy = findViewById(R.id.btn_DangKy);
+
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentDangNhap = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intentDangNhap);
+            }
         });
     }
 }
